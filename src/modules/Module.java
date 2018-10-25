f603aaaa4ebed5448ee5ee189993e58665739c07
marinouj <marinoujoanna@gmail.com>
2018-10-25 21:22:47 +0300

    coinReader,Module

--- src/modules/Module.java
+++ src/modules/Module.java
@@ -1,4 +1,11 @@
 package modules;
 
 abstract public class Module {
+	private String name;
+	
+	public Module (String name) {
+		this.name=name;
+	}
+	
+	public abstract String getName();
 }
--- src/modules/external/CoinReader.java
+++ src/modules/external/CoinReader.java
@@ -3,4 +3,15 @@ package modules.external;
 import modules.Module;
 
 public class CoinReader extends Module {
+	//private String name;
+	private int money;
+	
+	public CoinReader() {
+		super ("coinReader");
+		money = 0;
+	}
+	
+	public String getName() {
+		return "coinReader";
+	}
 }
