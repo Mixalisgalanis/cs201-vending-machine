a3a5f583cd363787087345f6bb2d37418efef177
Mgalanis <Mgalanis@users.noreply.github.com>
2018-11-15 14:42:21 +0200

    v1.0a5 - Added RecipeManagerTest

---
+++ src/recipes/RecipeManagerTest.java
@@ -0,0 +1,52 @@
+package recipes;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class RecipeManagerTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+
+    @org.junit.Test
+    public void getRecipes() {
+    }
+
+    @org.junit.Test
+    public void loadRecipes() {
+    }
+
+    @org.junit.Test
+    public void getRecipe() {
+    }
+
+    @org.junit.Test
+    public void getEnabled() {
+    }
+
+    @org.junit.Test
+    public void validateRecipes() {
+    }
+
+    @org.junit.Test
+    public void executeRecipe() {
+    }
+}
